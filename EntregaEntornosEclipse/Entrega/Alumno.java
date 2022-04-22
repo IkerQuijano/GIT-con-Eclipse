b18bab21158ee8b259913f3ab8673848cacda946
IkerQuijano <ikerquijano.daw@ciudadescolarfp.es>
2022-04-22 13:28:42 +0200

    Paso 3: Creo el constructor con parámetros.

--- EntregaEntornosEclipse/Entrega/Alumno.java
+++ EntregaEntornosEclipse/Entrega/Alumno.java
@@ -8,5 +8,14 @@ public class Alumno {
 	private String nombre;
 	private String apellido;
 	
+	
+//Constructor
 
+	public Alumno(String dni, String nombre, String apellido) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+	
 }
